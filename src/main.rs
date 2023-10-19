47c96698dde1da82c0ebb17087355eb63e77fe71
spruce <tungtose@gmail.com>
2023-10-19 22:32:55 +0700

    parse headers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
@@ -29,11 +30,32 @@ fn handle_connection(mut stream: TcpStream) {
     let reader = BufReader::new(&mut stream);
     let mut writer = BufWriter::new(stream_clone);
 
-    let request_line = reader.lines().next().unwrap().unwrap();
+    let http_request: Vec<_> = reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("HTTP: {:#?}", http_request);
+
+    let request_line = http_request.get(0).unwrap();
+
+    let mut headers_map: HashMap<String, String> = HashMap::new();
+
+    for line in http_request.iter() {
+        if line.contains(':') {
+            let splited = line.split(':').collect::<Vec<&str>>();
+            let key = splited.first().unwrap().trim().to_string();
+            let val = splited.get(1).unwrap().trim().to_string();
+            headers_map.insert(key, val);
+        }
+    }
 
     println!("Request line: {:#?}", request_line);
+    println!("Headers: {:#?}", headers_map);
 
     let echo_request = "/echo/";
+    let user_agent_request = "/user-agent";
     let ok_status = "HTTP/1.1 200 OK\r\n\r\n";
     let not_found_status = "HTTP/1.1 404 Not Found\r\n\r\n";
 
@@ -56,7 +78,17 @@ fn handle_connection(mut stream: TcpStream) {
         let response =
             format!("HTTP/1.1 200 OK\r\n{content_type}\r\n{content_length}\r\n\r\n{random_string}");
 
-        println!("Response: {:#?}", response);
+        writer.write_all(response.as_bytes()).unwrap();
+    } else if request_line.contains(user_agent_request) {
+        let user_agent_header = headers_map.get("User-Agent").unwrap();
+
+        let content_type = "Content-Type: text/plain";
+
+        let content_length = format!("Content-Length: {}", user_agent_header.len());
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\n{content_type}\r\n{content_length}\r\n\r\n{user_agent_header}"
+        );
 
         writer.write_all(response.as_bytes()).unwrap();
     } else {
