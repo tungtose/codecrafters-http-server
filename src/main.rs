024561ab288f10abfaae946afe5528b6c289a31e
spruce <tungtose@gmail.com>
2023-10-19 22:00:07 +0700

    response with content

--- src/main.rs
+++ src/main.rs
@@ -33,12 +33,33 @@ fn handle_connection(mut stream: TcpStream) {
 
     println!("Request line: {:#?}", request_line);
 
+    let echo_request = "/echo/";
+    let ok_status = "HTTP/1.1 200 OK\r\n\r\n";
+    let not_found_status = "HTTP/1.1 404 Not Found\r\n\r\n";
+
     if request_line == "GET / HTTP/1.1" {
-        let response = "HTTP/1.1 200 OK\r\n\r\n";
+        writer.write_all(ok_status.as_bytes()).unwrap();
+    } else if request_line.contains(echo_request) {
+        let splited_req = request_line.split(' ').collect::<Vec<&str>>();
+
+        let path = splited_req.get(1).unwrap();
+        let random_string = path
+            .split(echo_request)
+            .collect::<Vec<&str>>()
+            .pop()
+            .unwrap();
+
+        let content_type = "Content-Type: text/plain";
+
+        let content_length = format!("Content-Length: {}", random_string.len());
+
+        let response =
+            format!("HTTP/1.1 200 OK\r\n{content_type}\r\n{content_length}\r\n\r\n{random_string}");
+
+        println!("Response: {:#?}", response);
 
         writer.write_all(response.as_bytes()).unwrap();
     } else {
-        let response = "HTTP/1.1 404 Not Found\r\n\r\n";
-        writer.write_all(response.as_bytes()).unwrap();
+        writer.write_all(not_found_status.as_bytes()).unwrap();
     }
 }
